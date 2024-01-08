61a0b6c648a6d9c8a8f0be8337a0af62a9b62a79
Ashwin <ashwinkv.akv@gmail.com>
2024-01-08 20:56:55 +0530

    q3

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,15 +2,9 @@ import express from "express";
 import {userController} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import multer from "multer"
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-   callback(null, path.resolve(__dirname, "../uploads/"));
+    callback(null, "Backend/uploads");
   },
   filename: function (req, file, callback) {
     callback(null, Date.now() + "-" + file.originalname);
